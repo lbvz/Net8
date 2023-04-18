6c53da067ea80b0bb628b31d5642bc90fa94bdd9
lbvz <128460178+lbvz@users.noreply.github.com>
2023-04-19 02:30:33 +0800

    20230419

---
+++ src/範例_01/src/範例_01/範例_01.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package 範例_01;
+
+
+
+/**
+ *
+ * @author USER
+ */
+public class 範例_01 {
+    public static void main (String[]args){
+        //System.out.println("i love java");
+        //System.out.println("0000000");
+
+
+
+    //範例_02.顯示();
+    //範例_03.機算();
+    //範例_04.跑程式();
+    //範例_05.跑程式();
+    //範例_06.跑程式();
+    //比較.比較顯示();
+    //範例_07.跑程式();
+    銀行.跑();
+       
+   
+        
+        
+        
+        
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/範例_01/src/範例_01/範例_07.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package 範例_01;
+
+/**
+ *
+ * @author USER
+ */
+public class 範例_07 {
+    public static double a;
+    public static double b;
+    public static int eng;
+    public static int mr;
+    public static double x;
+    public static int y;
+    
+   public static void 設出值(){
+    System.out.println("---設出值---");   
+    a = Math.random(); 
+    b = Math.random();  
+  }
+  
+  public static void 計算(){
+      eng = (int)(a * 101.0);
+      mr = (int)(b * 101.0);
+      y = eng + mr;
+      x = y / 2;
+      System.out.println("===========執行============");
+  }
+  
+  public static void 顯示(){
+    System.out.println("亂數: "+ a);
+    System.out.println("亂數: "+ b);
+    System.out.println("英文成績 :"+ eng + "分");
+    System.out.println("數學成績 :"+ mr + "分");
+    System.out.println("總分 :"+ y + "分");
+    System.out.println("平均 :"+ x + "分");
+  }
+  
+  public static void 評價(){
+     if (x >= 90) {
+         System.out.println("平均 >= 90分 太棒");
+     }
+     else if (x >= 80){
+         System.out.println("平均 >= 80分 棒");
+     }
+     else if (x >= 70){
+         System.out.println("平均 >= 70分 好");
+     }
+     else if (x >= 60){
+         System.out.println("平均 >= 60分 加油");
+     }
+     else {
+         System.out.println("平均 < 60分 加加油");
+     }
+  }
+  public static void 跑程式(){
+      設出值();顯示();計算();顯示();評價();
+      
+  }
+}
+
