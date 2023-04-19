43b9f081cd5971b28c6b47c91e9a7536004c25c7
lbvz <128460178+lbvz@users.noreply.github.com>
2023-04-19 13:52:47 +0800

    20230419

--- src/範例_01/src/範例_01/範例_03.java
+++ src/範例_01/src/範例_01/範例_03.java
@@ -5,20 +5,20 @@ package 範例_01;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author USER
  */
 public class 範例_03 {
-    public static void 機算(){
-            System.out.println("英文成績:");
-            System.out.println(99);
-            System.out.println("數學成績:");
-            System.out.println(88);
-            System.out.println("總分:");
-            System.out.println(99+88);
-            
-}
-    
+
+    public static void 機算() {
+        System.out.println("英文成績:");
+        System.out.println(99);
+        System.out.println("數學成績:");
+        System.out.println(88);
+        System.out.println("總分:");
+        System.out.println(99 + 88);
+
+    }
+
 }
--- src/範例_01/src/範例_01/範例_04.java
+++ src/範例_01/src/範例_01/範例_04.java
@@ -13,50 +13,49 @@ public class 範例_04 {
     public static String 名字;
     public static int 數學成績;
     public static int 英文成績;
-    public static double 總分;
+    public static int 總分;
     public static double 平均;
-    
-  public static void 設定初值(){
-      名字="未命名";
-      數學成績=0;
-      英文成績=0;
-      總分=0;
-      平均=0;
-      
-      System.out.println("設定初值");
-     
-  }
-    public static void 計算總分(){
-      
-      
-      總分=數學成績+英文成績;
-      平均 = 總分/2;
-        System.out.println("計算總分平均"); 
-  }
-  public static void 顯示資料(){
-      System.out.println("顯示資料");
-      System.out.println("名字:"+名字);
-      System.out.println("數學成績:"+數學成績);
-      System.out.println("英文成績:"+英文成績);
-      System.out.println("總分:"+總分);
-      System.out.println("平均:"+平均);
-  }
-  public static void 修改資料(){
-      System.out.println("修改資料");
-      名字="Tom";
-      數學成績=99;
-      英文成績=88;
-      
-  }
 
+    public static void 設定初值() {
+        System.out.println("----設定初值----");
+        名字 = "未命名";
+        數學成績 = 0;
+        英文成績 = 0;
+        總分 = 0;
+        平均 = 0;
 
+    }
 
-          public static void 跑程式(){
-      設定初值();顯示資料();修改資料();計算總分();顯示資料();
-      
-      
-  }
+    public static void 計算總分() {
+        總分 = 數學成績 + 英文成績;
+        平均 = 總分 / 2;
+        System.out.println("----計算總分平均----");
+    }
 
+    public static void 顯示資料() {
+        System.out.println("========顯示資料========");
+        System.out.println("名字:" + 名字);
+        System.out.println("數學成績:" + 數學成績);
+        System.out.println("英文成績:" + 英文成績);
+        System.out.println("總分:" + 總分);
+        System.out.println("平均:" + 平均);
+    }
+
+    public static void 修改資料() {
+        System.out.println("----修改資料----");
+        名字 = "Tom";
+        數學成績 = 99;
+        英文成績 = 88;
+
+    }
+
+    public static void 跑程式() {
+        設定初值();
+        顯示資料();
+        修改資料();
+        計算總分();
+        顯示資料();
+
+    }
 
 }
-
